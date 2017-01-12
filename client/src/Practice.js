a7e38926597affdb785a81c0317f0738d74b7e60
ychang21 <isabellhchang@gmail.com>
2017-01-12 12:40:04 -0500

    messing with images and materialize

--- client/src/Practice.js
+++ client/src/Practice.js
@@ -5,30 +5,35 @@ class Practice extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {testAgain: 'failure'};
+    // this.state = {testAgain: 'failure'};
+    this.state = {
+      message: 'Hello! This is my component.',
+      src: "https://media.tenor.co/images/7131af15926a2a4eb4a740257a9a5a19/raw"
+    };
   }
 
 // SAMPLE FRONT-END REQUEST TO ACCESS API
-  testAgain = () => {
-    fetch('/')
-    .then(response => response.json())
-    .then(json => {
-      console.log(json);
-      this.setState({
-        testAgain: json.success
-      })
-    })
-  }
+  // testAgain = () => {
+  //   fetch('/')
+  //   .then(response => response.json())
+  //   .then(json => {
+  //     console.log(json);
+  //     this.setState({
+  //       testAgain: json.success
+  //     })
+  //   })
+  // }
 
 // TODO: Remove this.test() 
-  componentDidMount = () => {
-    this.testAgain();
-  }
+  // componentDidMount = () => {
+  //   this.testAgain();
+  // }
 
   render() {
     return (
       <div>
-        <p>Route success: {this.state.testAgain}</p>
+        <p className="blue-text text-darken-2 flow-text bold">{this.state.message}</p>
+        <img className="z-depth-5" src={this.state.src}/>
       </div>
     );
   }

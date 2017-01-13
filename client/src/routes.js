cbc006cbd4b1e3fa87a08cbef5bb358e4be1c70b
Mike Constanza <michael.constanza@gmail.com>
2017-01-12 20:36:21 -0500

    login route

--- client/src/routes.js
+++ client/src/routes.js
@@ -3,10 +3,12 @@ import React from 'react';
 import {Router, Route} from 'react-router';
 
 import App from './App';
+import Login from './Login';
 
 const Routes = (props) => (
     <Router {...props}>
         <Route path="/" component={App}>
+          <Route path="/login" component={Login}/>
         </Route>
     </Router>
 );

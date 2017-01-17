b6be86f5113f413f9ba2b3cb960551fe6fd08210
Yuuta Sumioka <ysumio120@yahoo.com>
2017-01-17 17:26:07 -0500

    building helpers.js

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -1,6 +1,33 @@
-
+import Twitch from '../config/Twitch';
 
 var helpers = {
+  buildQuery: function(headers) {
+    let header = [];
+
+    for(let key in headers) {
+       header.push(key + '=' + headers[key]); 
+    }
+
+    return header.join('&');
+  },
+
+  getToken: function(params, callback) {
+    return function() {
+      fetch("https://api.twitch.tv/kraken/oauth2/token?" + params, {
+        method: "POST"
+      })
+      .then((response) => response.json())
+      .then((data) => {
+        callback(data);
+      })
+    }
+  }
+
+  getTwitchUser: function(accessToken, callback) {
+    return function() {
+      
+    }
+  }
 
 }
 

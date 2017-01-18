f88a13e43bf1e5f946344281015c80ed86a81c31
Yuuta Sumioka <ysumio120@yahoo.com>
2017-01-17 20:46:59 -0500

    ...

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ app.use(logger(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:htt
 // ROUTES
 // ===================================================================
 
-app.get('/', function(req, res) {
+app.get('/*', function(req, res) {
 	res.sendFile(path.join(__dirname,'./build', 'index.html'));
 });
 

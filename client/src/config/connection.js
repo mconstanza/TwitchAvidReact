cee0923072b0831d30c27910bcd02dc151edc464
Mike Constanza <michael.constanza@gmail.com>
2017-01-27 15:00:48 -0500

    fixed database connection

--- client/src/config/connection.js
+++ client/src/config/connection.js
@@ -1,11 +1,6 @@
 import mongoose from 'mongoose';
 
-mongoose.connect("mongodb://heroku_n1g7kqk3:77i6igp969035jirr48l0h9h96@ds133279.mlab.com:33279/heroku_n1g7kqk3", function(err) {
+mongoose.connect("mongodb://heroku_s8llz6jn:hg7bu95ebfjiih7nc9cf8nf3hm@ds145188.mlab.com:45188/heroku_s8llz6jn", function(err) {
 	if(err) throw err;
 	console.log('database connected');
 });
-
-// mongoose.connect("mongodb://localhost:27017/twitchavid", function(err) {
-// 	if(err) throw err;
-// 	console.log('database connected');
-// });
